390f6c57a856451223326e61287bc84509c84de3
tiagoaugusto <112525927+TiagoAugustoSilva@users.noreply.github.com>
2024-07-25 11:08:27 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import VendingMachine from './VendingMachine'; 
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <VendingMachine />
+    </div>
+  );
+}
+
+export default App;
